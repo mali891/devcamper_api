76d73b102d73a2fd19db3a16036d2a6e2f843f0c
Mo Ali <m.ali23@hotmail.co.uk>
2020-08-06 19:21:19 +0100

    Connected MongoDB

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 
+const connectDB = require('./common/settings/db');
 const bootcamps = require('./routes/bootcamps');
 const URLS = require('./common/constants/urls');
 
+// Load env vars
 dotenv.config({ path: './common/settings/config.env' });
+
+// Connect to DB
+connectDB();
 
 const app = express();
 
+// Logging middleware
 if (process.env.NODE_ENV === 'dev') {
 	app.use(morgan('dev'));
 }
 
+// Connect bootcamp routes
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+const server = app.listen(
+	PORT,
+	console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.brightBlue.bold)
+);
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+	console.log(`Error: ${error.message}`.brightRed.bold);
+	server.close(() => process.exit(1));
+});
