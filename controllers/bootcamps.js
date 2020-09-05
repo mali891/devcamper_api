2958b42c6b139d0d2e57aa0f717cca000a31a9dc
Mo Ali <m.ali23@hotmail.co.uk>
2020-09-05 20:40:32 +0100

    Added getBootcampsInRadius controller and route

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../common/utils/errorResponse');
 const asyncHandler = require('../middleware/asyncHandler');
+const geocoder = require('../common/utils/geocoder');
 
 const successHandler = (res, data = null, message = null) => {
 	res.status(200).json({ success: true, message, data });
@@ -27,6 +28,31 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 **/
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	const bootcamps = await Bootcamp.find();
+
+	return bootcamps ? successHandler(res, bootcamps, null) : next(new ErrorResponse('No resources found', 404));
+});
+
+/**
+// @desc   - Get bootcamps within a specified radius
+// @route  - GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access - Public
+**/
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+	const { zipcode, distance } = req.params;
+
+	//Get lat/lng from geocoder
+	const location = await geocoder(process.env.GEOCODER_PROVIDER, process.env.GEOCODER_API_KEY).geocode(zipcode);
+	const latitude = location[0].latitude;
+	const longitude = location[0].longitude;
+	const earthRadius = 3963; // miles
+
+	// Calc radius using radians
+	// Divide distance by radius of earth
+	const radius = distance / earthRadius;
+
+	const bootcamps = await Bootcamp.find({
+		location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } }
+	});
 
 	return bootcamps ? successHandler(res, bootcamps, null) : next(new ErrorResponse('No resources found', 404));
 });
